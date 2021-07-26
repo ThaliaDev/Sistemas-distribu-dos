fff3a2b1a19ebfc133325934b2d90c6cac946466
Thalia Rodrigues <68879749+ThaliaDev@users.noreply.github.com>
2021-07-26 19:24:59 -0300

    Add files via upload

---
+++ ExerciciosSD21/MúltiplosPacotes/client.py
@@ -0,0 +1,18 @@
+#client.py
+
+#!/usr/bin/python                      # This is client.py file
+
+import socket                          # Import socket module
+
+s = socket.socket()                    # Create a socket object
+host = socket.gethostname()            # Get local machine name
+port = 12345                           # Reserve a port for your service.
+
+s.connect((host, port))
+print("1")
+data = s.recv(1024)
+print(data.decode())
+print("2")
+data = s.recv(1024)
+print(data.decode())
+s.close()   
