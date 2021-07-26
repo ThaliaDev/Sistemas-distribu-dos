f24f5a4d21d7ffcc1c3c18a4008d0bdc96b275bf
Thalia Rodrigues <68879749+ThaliaDev@users.noreply.github.com>
2021-07-26 19:17:34 -0300

    Add files via upload

---
+++ ExerciciosSD21/PingPong/cliente.py
@@ -0,0 +1,26 @@
+#client.py
+#!/usr/bin/python                               # This is client.py file
+
+import socket                                   # Import socket module
+
+s = socket.socket()                             # Create a socket object
+host = "10.0.2.15"                # Get local machine name
+port = 12345                                    # Reserve a port for your service.
+
+s.connect((host, port))
+
+while True:
+    print('Digite sua mensagem: ')
+    msg = input()
+
+    if msg == '':
+        print('Insira uma mensagem válida!')
+    elif msg == 'SAIR':
+        s.send(msg.encode())
+        break
+    else:
+        s.send(msg.encode())
+        response = s.recv(1024)
+        print(response.decode())
+
+s.close() 
